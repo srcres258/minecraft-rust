1f2c566ac717ee5188bc84663669327a4a9efc77
src_resources <src.res.211@gmail.com>
2024-06-10 11:34:24 +0800

    Add license headers

--- src/maths/general_maths.rs
+++ src/maths/general_maths.rs
@@ -1,3 +1,19 @@
+// SPDX-License-Identifier: Apache-2.0
+
+// Copyright 2024 src_resources
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 fn smooth_step(edge0: f32, edge1: f32, mut x: f32) -> f32 {
     // Scale, bias and saturate x to 0..1 range
     x = x * x * (3.0 - 2.0 * x);
--- src/world/world_constants.rs
+++ src/world/world_constants.rs
@@ -1,3 +1,19 @@
+// SPDX-License-Identifier: Apache-2.0
+
+// Copyright 2024 src_resources
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 pub const CHUNK_SIZE: usize = 16;
 pub const CHUNK_AREA: usize = CHUNK_SIZE * CHUNK_SIZE;
 pub const CHUNK_VOLUME: usize = CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE;
